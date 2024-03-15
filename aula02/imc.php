1130a9293679b717d16330e42496bbb0a32a3350
Henrique Conti <henriqueconti01@hotmail.com>
2024-03-14 22:26:06 -0300

    Exercicios e ex sobre loops

--- aula02/imc.php
+++ aula02/imc.php
@@ -1,13 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-<?php
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+
+        <?php
 $peso = 100;
 $altura = 1.79;
 $nome = "Conti";
@@ -33,5 +33,5 @@ switch ($imc) {
 }
    
 ?>
-</body>
+    </body>
 </html>
---
+++ aula03/maiorNumero.php
@@ -0,0 +1,5 @@
+<?php
+$mn = array(1, 2, 3, 4, 3, 10);
+      echo count($mn);
+
+?>
