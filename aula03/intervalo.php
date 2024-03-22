021b649464752be59d68071e125d9d8e57a93046
Henrique Conti <henriqueconti01@hotmail.com>
2024-03-21 22:29:56 -0300

    Aula de revisão

---
+++ aula03/intervalo.php
@@ -0,0 +1,11 @@
+<?php
+
+    for ($n = 30; $n <= 60; $n++) {
+        if ($n % 2 == 0) {
+            echo "par: $n <br>"; 
+        } else {
+            echo "Impar: $n <br>";
+        }
+}
+
+?>
--- aula03/maiorNumero.php
+++ aula03/maiorNumero.php
@@ -1,5 +1,15 @@
 <?php
-$mn = array(1, 2, 3, 4, 3, 10);
-      echo count($mn);
+$nums = [1, 22, 5, 4, 34, 122];
+$maiorNum = 0;
+
+      foreach ($nums as $num){
+
+            echo $num, "<br>";
+      
+            if ($num > $maiorNum){
+                  $maiorNum = $num;
+            }
+      }
+      echo "O maior número é: $maiorNum";
 
 ?>
