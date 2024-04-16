73a2e3c0393d94c8ce5c58242694c58628a3a7a2
Henrique Conti <henriqueconti01@hotmail.com>
2024-04-16 08:35:30 -0300

    Colocando aulas em ordem

--- aula03/fibonacci.php
+++ aula03/fibonacci.php
@@ -1,3 +1,29 @@
 <?php
+$num1 = 0;
+$num2 = 1;
 
+echo "Sequência Fibonacci com for loop: ";
+
+for($i = 0; $i < 10; $i++) {
+    echo $num1 . ", ";
+    $proxNum = $num1 + $num2;
+    $num1 = $num2;
+    $num2 = $proxNum;
+}
+?>
+
+<?php
+$num1 = 0;
+$num2 = 1;
+$contador = 0;
+
+echo "Sequência Fibonacci com while loop: ";
+
+while($contador < 10) {
+    echo $num1 . ", ";
+    $proxNum = $num1 + $num2;
+    $num1 = $num2;
+    $num2 = $proxNum;
+    $contador++;
+}
 ?>
